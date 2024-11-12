6cc17840468a5f2fa0bcb301f6d6768c6acca2ed
ZakEspley <zak.espley@gmail.com>
2024-11-11 19:45:53 -0800

    Updated for something I forgot about

--- lua/zak/init.lua
+++ lua/zak/init.lua
@@ -10,3 +10,20 @@ function ColorMyPencils(color)
 end
 
 ColorMyPencils()
+
+if vim.fn.has("wsl") then
+	vim.g.clipboard = {
+			   name= 'WslClipboard',
+			   copy= {
+			      ["+"]= 'win32yank.exe -i --crlf',
+			      ["*"]= 'win32yank.exe -i --crlf',
+			    },
+			   paste= {
+			      ["+"]= 'win32yank.exe -o --lf',
+			      ["*"]= 'win32yank.exe -o --lf',
+			   },
+			   cache_enabled= true,
+			  }
+--	  vim.keymap.set({"n", "v"}, "y", '"+y', { noremap = true, silent = true })
+--	  vim.keymap.set({"n", "v"}, "p", '"+p', { noremap = true, silent = true })
+end
